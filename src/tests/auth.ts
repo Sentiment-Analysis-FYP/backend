609cf40f3f5ae9cb76f5c15e697e5f79f63c5873
Max Otuteye <maxotuteye12@gmail.com>
2023-06-07 11:50:28 +0000

    adding babel for jest testing

---
+++ src/tests/auth.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import {app} from "../index";
+
+describe('Authentication Routes Tests', () => {
+    test('Sign Up Test', async () => {
+        const response = await request(app).post('/auth/register')
+        expect(response.body).toEqual({})
+    })
+});
